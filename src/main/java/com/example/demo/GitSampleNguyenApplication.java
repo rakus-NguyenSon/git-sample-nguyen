981b87236b1d1ac9f00b5f5dd817d307ac089ec0
rakus-NguyenSon <nhson@rakus.co.jp>
2021-05-20 12:35:20 +0900

    first commit

---
+++ src/main/java/com/example/demo/GitSampleNguyenApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleNguyenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleNguyenApplication.class, args);
+	}
+
+}
